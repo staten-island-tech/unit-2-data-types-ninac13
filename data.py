870dc0b17e39dafdefc1cfa966fc7f46d9ff4ff2
ninac13 <144480538+ninac13@users.noreply.github.com>
2023-09-20 14:01:29 -0400

    tips
    tips

--- data.py
+++ data.py
@@ -1,1 +1,3 @@
-input()
+values=[1,2.23,5,7,2,30,15]
+
+print(values[2])
---
+++ tips.py
@@ -0,0 +1,5 @@
+bill=input("Amount of bill: ")
+tip=input("Amount of tip: ")
+total=float(bill)+int(tip)
+
+print(float(bill)+int(tip))
