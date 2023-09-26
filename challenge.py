f076246b7ee9d55c3ce9aa8765fd5c4ed4d9e4e2
ninac13 <144480538+ninac13@users.noreply.github.com>
2023-09-26 13:59:35 -0400

    challenges

--- challenge.py
+++ challenge.py
@@ -1,5 +1,31 @@
-# sentence=input("Write a sentence: ")
-# broken=sentence.split()
-# print(len(broken))
-x = "test"
-print(f"hello {x}")
+def sent():
+    sentence=input("Write a sentence: ")
+    broken=sentence.split()
+    print(len(broken))
+
+sent()
+
+def num():
+    number=input("Write a number: ")
+    if int(number) % 2:
+        print("This number is odd")
+    else:
+        print("This number is even")
+
+num()
+
+def bill():
+    service=input("Was the service good, bad, okay, or great?: ")
+    if service == ("good"):
+        print("Pay a tip of 20%")
+    elif service == ("bad"):
+        print("Pay a tip of 0%")
+    elif service == ("okay"):
+        print("Pay a tip of 15%")
+    elif service == ("great"):
+        print("Pay a tip of 25%")
+
+bill()
+
+def factors(): 
+    number = input ("Please write a number: ")
--- madlibs.py
+++ madlibs.py
@@ -4,4 +4,4 @@ noun=input("Write a noun: ")
 number=input("Write a number: ")
 celebrity_guest=input("Name a celebrity: ")
 
-madlib=print(f"I wake up and realize immediately I'm late for school. I {verb1} to the bus and soon, I am at school. Phew. ")
+madlib=print(f"I wake up and realize immediately I'm late for {noun}. I {verb1} to the bus and then when I get to {noun}, I {verb2} to the room. 'Sorry I'm late!' I say to the teacher. 'You're excused, and the your test from last week is graded and on your desk,' my teacher replies. I {verb1} to my desk and after chatting with my best friend {celebrity_guest}, I check my test. 'No way...I got a {number}!")
