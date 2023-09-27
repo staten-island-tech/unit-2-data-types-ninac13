3e3c6d3407a2b5d938d13f7c633d77ae0776db74
ninac13 <144480538+ninac13@users.noreply.github.com>
2023-09-27 13:49:28 -0400

    eastbound and westbound traffic

--- challenge.py
+++ challenge.py
@@ -1,31 +1,33 @@
-def sent():
-    sentence=input("Write a sentence: ")
-    broken=sentence.split()
-    print(len(broken))
+# def sent():
+#     sentence=input("Write a sentence: ")
+#     broken=sentence.split()
+#     print(len(broken))
 
-sent()
+# sent()
 
-def num():
-    number=input("Write a number: ")
-    if int(number) % 2:
-        print("This number is odd")
-    else:
-        print("This number is even")
+# def num():
+#     number=input("Write a number: ")
+#     if int(number) % 2:
+#         print("This number is odd")
+#     else:
+#         print("This number is even")
 
-num()
+# num()
 
-def bill():
-    service=input("Was the service good, bad, okay, or great?: ")
-    if service == ("good"):
-        print("Pay a tip of 20%")
-    elif service == ("bad"):
-        print("Pay a tip of 0%")
-    elif service == ("okay"):
-        print("Pay a tip of 15%")
-    elif service == ("great"):
-        print("Pay a tip of 25%")
+# def bill():
+#     service=input("Was the service good, bad, okay, or great?: ")
+#     if service == ("good"):
+#         print("Pay a tip of 20%")
+#     elif service == ("bad"):
+#         print("Pay a tip of 0%")
+#     elif service == ("okay"):
+#         print("Pay a tip of 15%")
+#     elif service == ("great"):
+#         print("Pay a tip of 25%")
 
-bill()
+# bill()
 
 def factors(): 
     number = input ("Please write a number: ")
+
+
---
+++ traffic.py
@@ -0,0 +1,2 @@
+def traffic ():
+    eastbound=
